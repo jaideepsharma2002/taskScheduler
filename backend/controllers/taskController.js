9ad927b4fe113cee302dcb29f3f3755702a2fde1
jaideepsharma2002 <jaideepskillset@gmail.com>
2025-04-18 10:52:51 +0530

    added edit and delete functionality (#3)

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -39,11 +39,53 @@ const getAllTask = asyncHandler( async(req,res) => {
     catch(e) {
         res.status(500).json("Error in fetching the user tasks!");
     }
+})
 
 
 
+const editTask = asyncHandler( async (req,res) => {
+
+    try {
+
+        const taskid = req.params.id;
+        const {title} = req.body;
+
+        const updatedTask = await Task.findOneAndUpdate(
+            {_id: taskid},
+            {title},
+            {new: true}
+        );
+
+        if (!updatedTask) {
+            return res.status(404).json({message:"Task not found"});
+        }
+
+        res.json(updatedTask);
+    }
+    catch(e) {
+        res.status(500).json("error in updating task!");
+    }
+
+})
+
+const deleteTask = asyncHandler( async(req,res)=>{
+
+    try {
+
+        const task = await Task.findByIdAndDelete(req.params.id);
+
+        if (!task) {
+            return res.status(400).json("Task not found!");
+        }
+
+        return res.status(200).json("task deleted successfully");
+
+    }
+    catch (e){
+        return res.status(500).json("Error deleting the task");
+    }
 
 })
 
 
-module.exports = {createTask,getAllTask};
+module.exports = {createTask,getAllTask,editTask,deleteTask};
